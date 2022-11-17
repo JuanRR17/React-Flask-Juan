03a4c0f604c4fb82d58f5f904c7bd770a08899f5
JuanRR17 <juanrepeto@gmail.com>
2022-11-17 16:01:45 +0100

    database running

---
+++ src/app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask_migrate import Migrate
+from api.models import db
+from api.routes import api
+
+# create the app
+app = Flask(__name__)
+
+# configure the SQLite database, relative to the app instance folder (official Docs)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+#Define environment
+ENV = os.getenv("FLASK_ENV")
+
+# database configuration
+db_url = os.getenv("DATABASE_URL")
+if db_url is not None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+
+#to avoid warning message
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+MIGRATE = Migrate(app, db, compare_type = True)
+
+# initialize the app with the extension
+db.init_app(app)
+
+# Add all endpoints form the API with a "api" prefix
+app.register_blueprint(api, url_prefix='/api')
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
